f5ea54ccec60ffd8c25650d6c005642de8a74801
David Storm <jan.david.storm@gmail.com>
2014-06-16 15:23:11 -0400

    Update protocol version

--- p2pool/bitcoin/p2p.py
+++ p2pool/bitcoin/p2p.py
@@ -19,7 +19,7 @@ class Protocol(p2protocol.Protocol):
     
     def connectionMade(self):
         self.send_version(
-            version=70015,
+            version=70018,
             services=1,
             time=int(time.time()),
             addr_to=dict(
--- p2pool/networks.py
+++ p2pool/networks.py
@@ -25,7 +25,7 @@ nets = dict(
         WORKER_PORT=7903,
         BOOTSTRAP_ADDRS='drk.altmine.net drk2.altmine.net drk3.altmine.net asia01.poolhash.org asia02.poolhash.org q30.qhor.net poulpe.nirnroot.com drk.p2pool.n00bsys0p.co.uk darkcoin.kicks-ass.net darkcoin.fr cryptohasher.net coinminer.net drk.coinquarry.net drk.kopame.com 54.186.8.140 rebootcamp.de'.split(' '),
         ANNOUNCE_CHANNEL='#p2pool-drk',
-        VERSION_CHECK=lambda v: v >= 90411,
+        VERSION_CHECK=lambda v: v >= 91001,
     ),
     darkcoin_testnet=math.Object(
         PARENT=networks.nets['darkcoin_testnet'],
