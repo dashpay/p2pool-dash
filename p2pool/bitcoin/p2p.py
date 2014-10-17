9c4921ef286e04c03dcb4378887fd72747404aa6
Darkcoin <eduffield82+darkcoin@gmail.com>
2014-10-17 11:01:03 -0700

    version bump

--- p2pool/bitcoin/p2p.py
+++ p2pool/bitcoin/p2p.py
@@ -19,7 +19,7 @@ class Protocol(p2protocol.Protocol):
     
     def connectionMade(self):
         self.send_version(
-            version=70038,
+            version=70043,
             services=1,
             time=int(time.time()),
             addr_to=dict(
