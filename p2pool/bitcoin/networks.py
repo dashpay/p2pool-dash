c9dd9d6f12c9645aa06ed735a1be647f15f0711b
poiuty <poiuty@lepus.su>
2015-04-01 11:30:54 +0300

    fix explorer links

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -31,9 +31,9 @@ nets = dict(
         BLOCK_PERIOD=150, # s
         SYMBOL='DRK',
         CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'dash') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/dash/') if platform.system() == 'Darwin' else os.path.expanduser('~/.dash'), 'dash.conf'),
-        BLOCK_EXPLORER_URL_PREFIX='http://explorer.dash.io/block/',
-        ADDRESS_EXPLORER_URL_PREFIX='http://explorer.dash.io/address/',
-        TX_EXPLORER_URL_PREFIX='http://explorer.dash.io/tx/',
+        BLOCK_EXPLORER_URL_PREFIX='http://explorer.darkcoin.io/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://explorer.darkcoin.io/address/',
+        TX_EXPLORER_URL_PREFIX='http://explorer.darkcoin.io/tx/',
         SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**20 - 1),
         DUMB_SCRYPT_DIFF=1,
         DUST_THRESHOLD=0.001e8,
