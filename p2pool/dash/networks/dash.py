6d0bde9455e351f4096606e7dc3273f979ef9f2d
TheLazieR Yip <thelazier@gmail.com>
2016-12-14 11:19:25 -0500

    Update explorer url to dash.org

--- p2pool/dash/networks/dash.py
+++ p2pool/dash/networks/dash.py
@@ -21,8 +21,8 @@ POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('dash_hash').getPoWH
 BLOCK_PERIOD = 150 # s
 SYMBOL = 'DASH'
 CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Dash') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Dash/') if platform.system() == 'Darwin' else os.path.expanduser('~/.dash'), 'dash.conf')
-BLOCK_EXPLORER_URL_PREFIX = 'http://explorer.dashninja.pl/block/'
-ADDRESS_EXPLORER_URL_PREFIX = 'http://explorer.dashninja.pl/address/'
-TX_EXPLORER_URL_PREFIX = 'http://explorer.dashninja.pl/tx/'
+BLOCK_EXPLORER_URL_PREFIX = 'https://explorer.dash.org/block/'
+ADDRESS_EXPLORER_URL_PREFIX = 'https://explorer.dash.org/address/'
+TX_EXPLORER_URL_PREFIX = 'https://explorer.dash.org/tx/'
 SANE_TARGET_RANGE = (2**256//2**32//1000000 - 1, 2**256//2**32 - 1)
 DUST_THRESHOLD = 0.001e8
