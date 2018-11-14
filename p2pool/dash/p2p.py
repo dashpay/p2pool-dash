593a414f7a1e0e66809dfa933106ad2e5ac528ef
Alexander Block <ablock84@gmail.com>
2018-11-14 07:53:50 +0100

    Bump Dash proto version to latest

--- p2pool/dash/p2p.py
+++ p2pool/dash/p2p.py
@@ -19,7 +19,7 @@ class Protocol(p2protocol.Protocol):
 
     def connectionMade(self):
         self.send_version(
-            version=70208,
+            version=70212,
             services=1,
             time=int(time.time()),
             addr_to=dict(
