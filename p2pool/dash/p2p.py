61ea11e01e1294d0e1f59c0c16e01929d99fdc1b
UdjinM6 <UdjinM6@users.noreply.github.com>
2020-01-14 05:41:47 +0300

    Bump p2p version to 70216

--- p2pool/dash/p2p.py
+++ p2pool/dash/p2p.py
@@ -19,7 +19,7 @@ class Protocol(p2protocol.Protocol):
 
     def connectionMade(self):
         self.send_version(
-            version=70213,
+            version=70216,
             services=1,
             time=int(time.time()),
             addr_to=dict(
