6b37c0551154f7c309354c5df014d9c8588266d9
Robert LeBlanc <robert@leblancnet.us>
2020-02-08 10:49:58 -0800

    Fix copy paste error in testnet config.

--- p2pool/dash/networks/dash_testnet.py
+++ p2pool/dash/networks/dash_testnet.py
@@ -23,6 +23,6 @@ SYMBOL = 'tDASH'
 CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'DashCore') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/DashCore/') if platform.system() == 'Darwin' else os.path.expanduser('~/.dashcore'), 'dash.conf')
 BLOCK_EXPLORER_URL_PREFIX = 'https://testnet-insight.dashevo.org/insight/block/'
 ADDRESS_EXPLORER_URL_PREFIX = 'https://testnet-insight.dashevo.org/insight/address/'
-TX_EXPLORER_URL_PREFIX = 'https://testnet-insight.dashevo.org/insight/address/'
+TX_EXPLORER_URL_PREFIX = 'https://testnet-insight.dashevo.org/insight/tx/'
 SANE_TARGET_RANGE = (2**256//2**32//1000000 - 1, 2**256//2**20 - 1)
 DUST_THRESHOLD = 0.001e8
