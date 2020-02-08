cf598c5e645e210b43cdf7dbaca16f828c2c735c
Robert LeBlanc <robert@leblancnet.us>
2020-02-08 10:30:27 -0800

    Updated genesis block to correct block. Added testnet genesis block and updated block explorer.

--- p2pool/dash/networks/dash.py
+++ p2pool/dash/networks/dash.py
@@ -13,7 +13,7 @@ ADDRESS_VERSION = 76
 SCRIPT_ADDRESS_VERSION = 16
 RPC_PORT = 9998
 RPC_CHECK = defer.inlineCallbacks(lambda dashd: defer.returnValue(
-            (yield helper.check_block_header(dashd, '000007d91d1254d60e2dd1ae580383070a4ddffa4c64c2eeb4a2f9ecc0414343')) and
+            (yield helper.check_block_header(dashd, '00000ffd590b1485b3caadc19b22e6379c733355108f107a430458cdf3407ab6')) and
             (yield dashd.rpc_getblockchaininfo())['chain'] == 'main'
         ))
 BLOCKHASH_FUNC = lambda data: pack.IntType(256).unpack(__import__('dash_hash').getPoWHash(data))
